2882ebaec449a0256eef52c415f162f259226e61
Kent Ross <root.main@gmail.com>
2016-02-02 14:40:42 -0800

    completed styling todos

--- class-02-jquery-and-events/rick-kent/scripts/articleView.js
+++ class-02-jquery-and-events/rick-kent/scripts/articleView.js
@@ -29,24 +29,30 @@ articleView.populateFilters = function() {
 
 articleView.handleAuthorFilter = function() {
   $('#author-filter').on('change', function() {
+    // Done: Filter shown articles by data-author attribute
     if ($(this).val()) {
       $("article[data-author='" + $(this).val() + "']").fadeIn();
       $("article:not([data-author='" + $(this).val() + "'])").fadeOut();
     } else {
+      // If nothing is selected show all articles
       $('article:not(.template)').fadeIn();
     }
+    // Reset the value of the category filter
     $('#category-filter').val('');
   });
 };
 
 articleView.handleCategoryFilter = function() {
   $('#category-filter').on('change', function() {
+    // Done - Filter shown articles by category
     if ($(this).val()) {
       $("article[data-category='" + $(this).val() + "']").fadeIn();
       $("article:not([data-category='" + $(this).val() + "'])").fadeOut();
     } else {
+      // If nothing is selected show all articles
       $('article:not(.template)').fadeIn();
     }
+    // Reset the value of the author filter
     $('#author-filter').val('');
   });
 };
@@ -54,17 +60,21 @@ articleView.handleCategoryFilter = function() {
 articleView.handleMainNav = function() {
 
   $('.main-nav').on('click', 'li.tab', function() {
+    // Show only the section for the tab we clicked on
     $('section.tab-content').hide();
     $('section.tab-content#' + $(this).attr('data-content')).show();
-
   });
 
-  $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
+  // Let's now trigger a click on the first .tab element, to set up the page.
+  $('.main-nav .tab:first').click();
 };
 
 articleView.setTeasers = function() {
+  // Hide every element after the second in each article's content
   $('.article-body *:nth-of-type(n+2)').hide();
   $('.read-on').click(function(e) {
+    // When read-on link is clicked, don't follow the link but instead toggle
+    //   the view to hide this link and show everything in the article's content
     e.preventDefault();
     $(this).hide().siblings('.article-body').find('*').show();
   });
@@ -72,6 +82,7 @@ articleView.setTeasers = function() {
 
 
 $(function(){
+  // Done - On ready populate our filter boxes and attach all handles
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
