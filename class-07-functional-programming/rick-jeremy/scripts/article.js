1e06766053b8030bf49519aac59338a6d8c2b122
Jeremy Edwards <jeremytedwards@gmail.com>
2016-02-09 15:55:26 -0800

    Added code to implement numWordsByAuthor Additional Clean Up: Wired up the Handlebars template for #author-template; Cleaned up esLint spacing issues in article.js; Missing semicolon line 106 col 40; Missing semicolon line 125 col 7;

--- class-07-functional-programming/rick-jeremy/scripts/article.js
+++ class-07-functional-programming/rick-jeremy/scripts/article.js
@@ -1,107 +1,98 @@
-// Done: Wrap the entire contents of this file in an IIFE.
+// DONE: Wrap the entire contents of this file in an IIFE.
 // Pass in to the IIFE a module, upon which objects can be attached for later access.
 (function(module) {
 
-
-
-function Article (opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
-}
-
-Article.all = [];
-
-Article.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#article-template').text());
-
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  this.body = marked(this.body);
-
-  return template(this);
-};
-
-Article.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  // DONE: Refactor this forEach code, by using a `.map` call instead, since want we are trying to accomplish
-  // is the transformation of one colleciton into another.
-  // rawData.forEach(function(ele) {
-  //   Article.all.push(new Article(ele));
-  // })
-  Article.all = rawData.map(function(ele) {
-    return new Article(ele);
-  });
-};
-
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
-// TODO: Refactor this function, so it accepts an argument of a callback function (likely a view function)
-// to execute once the loading of articles is done.
-Article.fetchAll = function(callback, callbackObj){
-  if (callbackObj) {
-    Article.loadAll(JSON.parse(callbackObj));
-    callback();
-  } else {
-    $.getJSON('/data/hackerIpsum.json', function(rawData) {
-      Article.loadAll(rawData);
-      callbackObj = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
-      callback();
-    });
+  function Article (opts) {
+    this.author = opts.author;
+    this.authorUrl = opts.authorUrl;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.body = opts.body;
+    this.publishedOn = opts.publishedOn;
   }
-};
-// Article.fetchAll = function() {
-//   if (localStorage.rawData) {
-//     Article.loadAll(JSON.parse(localStorage.rawData));
-//     articleView.initIndexPage();
-//   } else {
-//     $.getJSON('/data/hackerIpsum.json', function(rawData) {
-//       Article.loadAll(rawData);
-//       localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
-//       articleView.initIndexPage();
-//     });
-//   }
-// };
 
-// DONE: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
-Article.numWordsAll = function() {
-  return Article.all.map(function(article) {
-    return article.body.match(/\b\w+/g).length;
-  })
-  .reduce(function(a, b) {
-    return a + b;
-  })
-};
+  Article.all = [];
 
-// DONE: Chain together a `map` and a `reduce` call to produce an array of unique author names.
-Article.allAuthors = function() {
-   return Article.all.map(function(article) {
-    //  console.log(article.author);
-	  return article.author;
-  })
-  .reduce(function(a,b){
-    console.log(a, b)
-    if (a.indexOf(b) < 0 ) a.push(b);
-    return a;
-  },[])
-};
+  Article.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#article-template').text());
 
-Article.numWordsByAuthor = function() {
-  // TODO: Transform each author string into an object with 2 properties: One for
-  // the author's name, and one for the total number of words across all articles written by the specified author.
-  return Article.allAuthors().map(function(author) {
-    return {
-      // someKey: someValOrFunctionCall().map(...).reduce(...), ...
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = marked(this.body);
+
+    return template(this);
+  };
+
+  Article.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    // DONE: Refactor this forEach code, by using a `.map` call instead, since want we are trying to accomplish
+    // is the transformation of one colleciton into another.
+    // rawData.forEach(function(ele) {
+    //   Article.all.push(new Article(ele));
+    // })
+    Article.all = rawData.map(function(ele) {
+      return new Article(ele);
+    });
+  };
+
+  // This function will retrieve the data from either a local or remote source,
+  // and process it, then hand off control to the View.
+  // DONE: Refactor this function, so it accepts an argument of a callback function (likely a view function)
+  // to execute once the loading of articles is done.
+  Article.fetchAll = function(callback, callbackObj){
+    if (callbackObj) {
+      Article.loadAll(JSON.parse(callbackObj));
+      callback();
+    } else {
+      $.getJSON('/data/hackerIpsum.json', function(rawData) {
+        Article.loadAll(rawData);
+        callbackObj = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
+        callback();
+      });
     }
-  })
-};
+  };
+
+  // DONE: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
+  var numWords = function(a) {
+    return a.map(function(article) {
+      return article.body.match(/\b\w+/g).length;
+    })
+    .reduce(function(a, b) {
+      return a + b;
+    });
+  };
+
+  Article.numWordsAll = function() {
+    return numWords(Article.all);
+  };
+
+  // DONE: Chain together a `map` and a `reduce` call to produce an array of unique author names.
+  Article.allAuthors = function() {
+    return Article.all.map(function(article) {
+      //  console.log(article.author);
+      return article.author;
+    })
+    .reduce(function(a,b){
+      if (a.indexOf(b) < 0 ) a.push(b);
+      return a;
+    },[]);
+  };
+
+  Article.numWordsByAuthor = function() {
+    // DONE: Transform each author string into an object with 2 properties: One for
+    // the author's name, and one for the total number of words across all articles written by the specified author.
+    return Article.allAuthors().map(function(author) {         // get unique set authors
+      return {
+        name: author,
+        count: numWords(Article.all.filter(function(ele) {
+          return ele.author == author;
+        }))};
+    });
+  };
 
 
-module.Article = Article;
+  module.Article = Article;
 }(window));
